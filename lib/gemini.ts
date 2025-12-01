583829a19ceaf803a3dbb58b397a12266d885d09
Lucasfc9812 <xboxliveduli@gmail.com>
2025-12-01 05:43:13 -0300

    Fix DEFINITIVO: gemini-pro-vision

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -3,4 +3,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey || "");
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-002" });
+export const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
