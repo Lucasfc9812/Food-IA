44cb2afac3114e98e904b68551c886b3aa3b1d0a
Lucasfc9812 <xboxliveduli@gmail.com>
2025-12-01 05:21:24 -0300

    Tentando modelo text-preview

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: Request) {
                     ],
                 },
             ],
-            model: "llama-3.2-11b-vision-preview",
+            model: "llama-3.2-11b-text-preview",
             temperature: 0.1,
             max_tokens: 1024,
             top_p: 1,
