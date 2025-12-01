aaab247fdba823c2ccd3999171c6f6ac69e35350
Lucasfc9812 <xboxliveduli@gmail.com>
2025-12-01 04:49:28 -0300

    Mudando para Gemini 1.5 Pro

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -3,4 +3,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.GOOGLE_API_KEY || "dummy-key";
 const genAI = new GoogleGenerativeAI(apiKey);
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-001" });
+export const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
